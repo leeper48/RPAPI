530b047731fa5281326b71786ba3fa22b2c478fc
Kurt Jordan <kjordan@premediaglobal.com>
2013-03-30 16:25:57 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RPAPI::Application.config.secret_token = '44cc8dabfd3bd9a69a2e44193c32389beafc80a925343448c2f440ec3d0025ac6332c82d2d7437a7680294179d0f9f938a7b45550d99a418b527194368aa5842'
