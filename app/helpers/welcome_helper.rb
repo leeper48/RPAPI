bae4cf398836ce830d86ca5a4733aaf3e46db48a
Kurt Jordan <kjordan@premediaglobal.com>
2013-03-30 17:24:29 -0400

    UI update

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,14 @@
 module WelcomeHelper
+  def sessiontoken()
+    require './app/models/create_session_sample.rb'
+    sess = CreateSessionSample.new
+    sess.create_session
+  end
+
+  def imgsearch(schphrase)
+    require './app/models/search_for_images_sample.rb'
+    @imgsch = SearchForImagesSample.new
+    @found = @imgsch.search_for_images(sessiontoken, schphrase)
+
+  end
 end
